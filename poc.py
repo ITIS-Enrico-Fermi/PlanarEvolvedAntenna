41971a190e08d40e336a2058e55b63706bce5b26
mc-cat-tty <44820563+mc-cat-tty@users.noreply.github.com>
2023-05-07 16:45:56 +0200

    WIP

--- poc.py
+++ poc.py
@@ -1,6 +1,9 @@
-from config import Config
 import numpy as np
+from random import randrange
+from config import Config
 from typing import List
+
+CONFIG_FILENAME = "config.yaml"
 
 class Gene:
   def __init__(self):
@@ -11,6 +14,16 @@ class Gene:
     self.revolutionAngles = \
       (np.random.rand(Config.GeneEncoding.segmentsNumber - 1) - 0.5) * np.pi # 1 joint every segments' couple
 
+  def __lt__(self, other) -> bool:
+    return self.fitness() < other.fitness()
+  
+  def __repr__(self) -> str:
+    res = ""
+    for i in range(1, self.revolutionAngles.size):
+      res += f"{self.segmentLengths[i-1]} mm - {self.revolutionAngles[i]} deg - "
+    res += f"{self.segmentLengths[i-1]} mm"
+
+    return res
 
   def isValid(self) -> bool:
     """Returns true if the path is not slef-intersecting
@@ -26,6 +39,7 @@ class Gene:
 class Population:
   def __init__(self):
     self.population = [Gene() for _ in range(Config.GeneticAlgoTuning.populationSize)]
+    self.generationNumber = 0
 
   def nextGeneration(self) -> List[Gene]:
     for _ in range(Config.GeneticAlgoTuning.iterationsNumber):
@@ -33,6 +47,8 @@ class Population:
       self.crossover()
       self.mutate()
       yield self.population
+    
+    raise StopIteration(f"Reached {Config.GeneticAlgoTuning.iterationsNumber} iterations")
   
   def fight(self):
     """This step filters out non-valid individuals and
@@ -43,17 +59,25 @@ class Population:
       self.population
     )
 
-    
+    newGenerationSize = Config.GeneticAlgoTuning.turnoverRate * Config.GeneticAlgoTuning.populationSize
+    self.population = sorted(self.population)[ : int(newGenerationSize)]
   
   def crossover(self):
-    ...
+    jointsNumber = Config.GeneEncoding.segmentsNumber - 1
+    cutpointIdx = randrange(jointsNumber)
+
   
   def mutate(self):
     ...
 
 
 def main():
-  ...
+  with open(CONFIG_FILENAME, "r") as f:
+    Config.loadYaml(f)
+    
+  pop = Population()
+  for generation in pop.nextGeneration():
+    print(generation) 
 
 
 if __name__ == "__main__":
