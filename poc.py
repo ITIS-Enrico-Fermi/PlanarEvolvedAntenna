8522f23c20d1db59b504a201c1177a62fde693d5
mc-cat-tty <44820563+mc-cat-tty@users.noreply.github.com>
2023-05-07 15:31:28 +0200

    wip

--- poc.py
+++ poc.py
@@ -4,8 +4,13 @@ from typing import List
 
 class Gene:
   def __init__(self):
-    self.segmentLengths = np.random.rand(Config.GeneEncoding.segmentsNumber)  * Config.ShapeConstraints.outerDiam / (Config.GeneEncoding.segmentsNumber * 4)
-    self.revolutionAngles = np.random.rand(Config.GeneEncoding.segmentsNumber - 1) * np.pi # 1 joint every segments' couple
+    self.segmentLengths = \
+      np.random.rand(Config.GeneEncoding.segmentsNumber)  * \
+      Config.ShapeConstraints.outerDiam / (Config.GeneEncoding.segmentsNumber * 4)
+    
+    self.revolutionAngles = \
+      (np.random.rand(Config.GeneEncoding.segmentsNumber - 1) - 0.5) * np.pi # 1 joint every segments' couple
+
 
   def isValid(self) -> bool:
     """Returns true if the path is not slef-intersecting
@@ -13,20 +18,38 @@ class Gene:
     """
     ...
   
+  
   def fitness(self) -> np.float16:
     ...
 
 
-class Generation:
+class Population:
   def __init__(self):
     self.population = [Gene() for _ in range(Config.GeneticAlgoTuning.populationSize)]
 
-  def iterate(self) -> List[Gene]:
+  def nextGeneration(self) -> List[Gene]:
     for _ in range(Config.GeneticAlgoTuning.iterationsNumber):
       self.fight()
       self.crossover()
       self.mutate()
       yield self.population
+  
+  def fight(self):
+    """This step filters out non-valid individuals and
+    keeps only some of them according to the turnover rate
+    """
+    self.population = filter(
+      lambda x: x.isValid(),
+      self.population
+    )
+
+    
+  
+  def crossover(self):
+    ...
+  
+  def mutate(self):
+    ...
 
 
 def main():
