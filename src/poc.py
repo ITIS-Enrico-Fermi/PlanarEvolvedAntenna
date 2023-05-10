0d458f64808cf5e462be6c67aa1aa192f82d924a
giuliocorradini <39673120+giuliocorradini@users.noreply.github.com>
2023-05-10 15:22:49 +0200

    Changed population nextGeneration method name First edits for matplotlib animation

--- src/poc.py
+++ src/poc.py
@@ -2,20 +2,49 @@ import argparse
 from utils.amenities import plotPath
 from config import Config
 from population import Population
+from functools import partial
+from typing import Callable
 
 CONFIG_FILENAME = "config.yaml"
 
+def simulationStep(pop: Population, doPlot: bool, *_) -> None:
+  generation, epoch = next(pop.generations())
+
+  print(epoch, generation)
+
+  if doPlot:
+    plotPath(f"Epoch: {epoch}", generation[0].getCartesianCoords())  # Plot only best performing individual
+
+  return
+
+
+def buildSimulation(doPlot: bool, *_) -> Callable[[Population, bool], None]:
+  pop = Population()
+
+  return partial(simulationStep, pop, doPlot)
+
+
 def main(doPlot: bool):
+  epochs = 20
+
   with open(CONFIG_FILENAME, "r") as f:
     Config.loadYaml(f)
-    
-  pop = Population()
-  for generation, epoch in pop.nextGeneration():
-    print(epoch, generation)
 
-    if doPlot:
-      import matplotlib.pyplot as plt
-      plotPath(f"Epoch: {epoch}", generation[0].getCartesianCoords())  # Plot only best performing individual
+  simulation = buildSimulation(doPlot)
+    
+  if doPlot:
+    import matplotlib.pyplot as plt
+    import matplotlib.animation as animation
+
+    simulation_callback = partial(simulation, doPlot)
+
+    fig = plt.figure()
+    anim = animation.FuncAnimation(fig, simulation, interval=200)
+    plt.show()
+
+  else:
+    for e in range(epochs):
+      simulation()
 
 
 if __name__ == "__main__":
@@ -27,7 +56,7 @@ if __name__ == "__main__":
     "-p", "--plot", help="View what's happening in the simulation",
     default=False, action="store_true"
   )
-  
+
   args = parser.parse_args()
 
   main(args.plot)
