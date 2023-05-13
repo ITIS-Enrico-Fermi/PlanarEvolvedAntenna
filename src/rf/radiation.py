9e40819ded6f97576223992031999c9a3c1b8f6a
mc-cat-tty <44820563+mc-cat-tty@users.noreply.github.com>
2023-05-13 11:20:03 +0200

    toCamelCase

--- src/rf/radiation.py
+++ src/rf/radiation.py
@@ -5,16 +5,16 @@ from necpp import *
 @dataclass
 class RadiationPattern():
     """Class to represent a radiation pattern. Computation output of NEC"""
-    gains_db: List[float]
+    gainsDb: List[float]
     thetas: List[float]
     phis: List[float]
 
     @classmethod
     def from_nec_context(self, ctx):
-        gains_db = [nec_gain(ctx, 0, i, 0) for i in range(0, 12)]
-        #self.gains = list(map(lambda g: 10.0**(g / 10.0), self.gains_db))
+        gainsDb = [nec_gain(ctx, 0, i, 0) for i in range(0, 12)]
+        #self.gains = list(map(lambda g: 10.0**(g / 10.0), self.gainsDb))
         thetas = [i * 15 * 3.1415 / 180.0 for i in range(0, 12)]
         phis = [0]*12
 
-        return RadiationPattern(gains_db, thetas, phis)
+        return RadiationPattern(gainsDb, thetas, phis)
     
