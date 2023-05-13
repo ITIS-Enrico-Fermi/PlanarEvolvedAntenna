5892ec839f89f01c9eb28d9926de243be836504b
mc-cat-tty <44820563+mc-cat-tty@users.noreply.github.com>
2023-05-13 14:03:07 +0200

    WIP persistence

--- src/poc.py
+++ src/poc.py
@@ -11,7 +11,11 @@ from functools import partial
 
 CONFIG_FILENAME = "config.yaml"
 
-def simulationStep(pop: Population, doPlot: bool, shape_axes: plt.Axes, radiation_axes: plt.Axes, *_) -> None:
+def simulationStep(
+  pop: Population,
+  doPlot: bool,
+  *_,
+  **kwargs) -> None:
   generation, epoch = next(pop.generations())
   
   logging.info(f"Epoch: {epoch}")
@@ -23,19 +27,23 @@ def simulationStep(pop: Population, doPlot: bool, shape_axes: plt.Axes, radiatio
       title=f"Epoch: {epoch} - Fitness: {generation[0].fitness():.2f}",
       polychain=generation[0].getCartesianCoords(),  # Plot only best performing individual
       radiation=generation[0].getRadiationPattern(),
-      axes=(shape_axes, radiation_axes)
+      axes=(kwargs.pop("shapeAxes"), kwargs.pop("radiationAxes"))
     )
+  
+  outputDirectory = kwargs.pop("outputDirectory")
+  if outputDirectory is not None:
+    print(outputDirectory)
 
 
-def buildSimulation(doPlot: bool, *args) -> Callable[[Population, bool], None]:
+def buildSimulation(doPlot: bool, *_, **kwargs) -> Callable[[Population, bool], None]:
   pop = Population()
 
   signal.signal(signal.SIGINT, lambda *_: print(f"King is: {pop.king}"))
 
-  return partial(simulationStep, pop, doPlot, *args)
+  return partial(simulationStep, pop, doPlot, **kwargs)
 
 
-def main(doPlot: bool):
+def main(doPlot: bool, outdir: str):
 
   logging.basicConfig(
     level=logging.INFO,
@@ -45,18 +53,17 @@ def main(doPlot: bool):
 
   with open(CONFIG_FILENAME, "r") as f:
     Config.loadYaml(f)
-
     
   if doPlot:
     fig = plt.figure()
     shape = fig.add_subplot(1, 2, 1)
     radPattern = fig.add_subplot(1, 2, 2, projection='polar')
-    simulation = buildSimulation(doPlot, shape, radPattern)
-    anim = animation.FuncAnimation(fig, simulation, interval=10)
+    simulation = buildSimulation(doPlot, shapeAxes = shape, radiationAxes = radPattern, outputDirectory = outdir)
+    anim = animation.FuncAnimation(fig, simulation, interval=10, cache_frame_data=False)
     plt.show()
-
+    
   else:
-    simulation = buildSimulation(doPlot)
+    simulation = buildSimulation(doPlot, outputDirectory = outdir)
     while True:
       simulation()
 
@@ -71,6 +78,11 @@ if __name__ == "__main__":
     default=False, action="store_true"
   )
 
+  parser.add_argument(
+    "-o", "--outdir", help="Save each generation to an svg file inside outdir. May slow down the simulation.",
+    type=str, default=None
+  )
+
   args = parser.parse_args()
 
-  main(args.plot)
+  main(args.plot, args.outdir)
