a3956ca69872e096b66fc9472b8376030af0abb9
mc-cat-tty <44820563+mc-cat-tty@users.noreply.github.com>
2023-05-14 12:17:03 +0200

    2-spaces indentation

--- src/rf/radiation.py
+++ src/rf/radiation.py
@@ -4,17 +4,17 @@ from necpp import *
 
 @dataclass
 class RadiationPattern():
-    """Class to represent a radiation pattern. Computation output of NEC"""
-    gainsDb: List[float]
-    thetas: List[float]
-    phis: List[float]
+  """Class to represent a radiation pattern. Computation output of NEC"""
+  gainsDb: List[float]
+  thetas: List[float]
+  phis: List[float]
 
-    @classmethod
-    def from_nec_context(self, ctx):
-        gainsDb = [nec_gain(ctx, 0, i, 0) for i in range(0, 12)]
-        #self.gains = list(map(lambda g: 10.0**(g / 10.0), self.gainsDb))
-        thetas = [i * 15 * 3.1415 / 180.0 for i in range(0, 12)]
-        phis = [0]*12
+  @classmethod
+  def from_nec_context(self, ctx):
+    gainsDb = [nec_gain(ctx, 0, i, 0) for i in range(0, 12)]
+    #self.gains = list(map(lambda g: 10.0**(g / 10.0), self.gainsDb))
+    thetas = [i * 15 * 3.1415 / 180.0 for i in range(0, 12)]
+    phis = [0]*12
 
-        return RadiationPattern(gainsDb, thetas, phis)
+    return RadiationPattern(gainsDb, thetas, phis)
     
