1f55cd18b8dad0789633946800201fb1a7a58ffa
giuliocorradini <39673120+giuliocorradini@users.noreply.github.com>
2023-05-24 22:08:29 +0200

    Added niching mechanism and copied mutation

--- src/core/niche.py
+++ src/core/niche.py
@@ -1,3 +1,4 @@
+from typing import List
 from core.population import Population
 from core.gene import Gene
 from config import Config
@@ -5,9 +6,10 @@ from random import choices
 import numpy as np
 from operator import itemgetter
 from random import randrange
+import logging
 
 class NichePopulation(Population):
-    def __init__(self):
+    def __init__(self, mutationRate: float):
         super().__init__(Config.GeneticAlgoTuning.worldHeight * Config.GeneticAlgoTuning.worldWidth)
         
         self.world: np.ndarray = np.array(self.population).reshape(
@@ -15,44 +17,102 @@ class NichePopulation(Population):
             Config.GeneticAlgoTuning.worldWidth
         )
 
-    def extractParents(self, kernel: np.matrix):
+        self.mutationRate = mutationRate
+
+    def populationSet(self) -> np.ndarray:
+        # TODO: unify this property with superclass population attribute
+        return self.world.reshape(self.world.size)
+
+    def extractParents(self, neigh: np.matrix):
         """
-        Extract parents for crossover from a kernel (submatrix of world).
+        Extract parents for crossover from a neighbourhood (submatrix of world).
         """
 
-        submatrix = kernel.tolist()
+        neigh = neigh.reshape(neigh.size)
 
-        fitness = [g.fitness() for g in submatrix]
-        fitness = [500 + f if f > float("-inf") else 0 for f in fitness]
+        fitness = [g.fitness() for g in neigh]
+        fitness = [100 + f if f > float("-inf") else 0 for f in fitness]
 
-        return choices(submatrix, weights=fitness, k=2)
+        return choices(neigh, weights=fitness, k=2)
     
     def generateOffspring(self):
         rows, cols = self.world.shape
         
-        x, y = randrange(...)
+        x = randrange(rows)
+        y = randrange(cols)
 
-        submatrix = self.world[i:i+kernel_dim, j:j+kernel_dim]
+        neigh = self.world[x-1:x+2, y-1:y+2]
 
-        mother, father = self.extractParents(submatrix)
+        mother, father = self.extractParents(neigh)
         childA, childB = self.crossover(mother, father)
 
-        for x, y, competitor in sorted(np.ndenumerate(submatrix), key=itemgetter(2)):
-            if(childA > competitor):
-                submatrix[x][y] = childA
-                break
-        
-        for x, y, competitor in sorted(np.ndenumerate(submatrix), key=itemgetter(2)):
-            if(childB > competitor):
-                submatrix[x][y] = childB
+        self.mutate(childA)
 
-    def mutate():
-        pass
+        if not childA.isValid():
+            return
+
+        (x, y), weakest = min(np.ndenumerate(neigh), key=itemgetter(1))
+        if childA > weakest:
+            neigh[x][y] = childA
+            print(f"Sub ({x}, {y}) with child A with fitness: {childA}")
+        
+    def mutate(self, gene: Gene):
+        mutationAngles = (np.random.rand(Config.GeneEncoding.segmentsNumber) - 0.5) \
+        * Config.GeneEncoding.maxAngle
+
+        mutationLengths = np.random.uniform(
+            low = - (Config.GeneEncoding.minSegmentLen + Config.GeneEncoding.maxSegmentLen) / 2,
+            high = (Config.GeneEncoding.minSegmentLen + Config.GeneEncoding.maxSegmentLen) / 2,
+            size = Config.GeneEncoding.segmentsNumber
+        )
+
+        mutationGpDistance = np.random.uniform(
+            low = Config.ShapeConstraints.groundPlaneDistanceMin,
+            high = Config.ShapeConstraints.groundPlaneDistanceMax,
+            size = 1
+        )[0]
+
+        newAngles = np.clip(
+            gene.getAngleArray() + mutationAngles,
+            - Config.GeneEncoding.maxAngle / 2,
+            + Config.GeneEncoding.maxAngle / 2
+        )
+
+        newLengths = np.clip(
+            gene.getLengthArray() + mutationLengths,
+            Config.GeneEncoding.minSegmentLen,
+            Config.GeneEncoding.maxSegmentLen
+        )
+
+        gene.setEncoding(newAngles, newLengths)
+        gene.setGroundPlaneDistance(mutationGpDistance)
+            
 
     def fight():
         pass
 
+    def generations(self) -> List[Gene]:
+        for _ in range(Config.GeneticAlgoTuning.iterationsNumber):
+            self.generateOffspring()
+
+            self.fitnessMean = np.mean([g.fitness() for g in self.populationSet()])
+            self.fitnessStdDev = np.std([g.fitness() for g in self.populationSet()])
+            logging.info(
+                f"\nFitness:\n"
+                f"\tMean: {self.fitnessMean:.4f}\n"
+                f"\tSd: {self.fitnessStdDev:.4f}\n"
+                f"Population size: {self.world.size}"
+            )
+
+            self.king = max(self.populationSet())
+
+            if self.fitnessStdDev <= np.finfo(np.float32).eps:
+                return
+
+            self.generationNumber += 1
+            yield sorted(self.populationSet().tolist(), reverse=True), self.generationNumber
+
 
 if __name__ == '__main__':
-    p = NichePopulation()
+    p = NichePopulation(0.05)
     p.generateOffspring()
